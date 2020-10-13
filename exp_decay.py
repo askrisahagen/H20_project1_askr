98a0395adca0d861f74a9baa0c5f9b0a63e9f393
Ask Risahagen <askr@student.matnat.uio.no>
2020-10-14 01:46:06 +0200

    Made a change

--- exp_decay.py
+++ exp_decay.py
@@ -1,40 +1,14 @@
+import scipy
+from scipy.integrate import solve_ivp
+import numpy as np
+
 class ExponentialDecay:
     def __init__(self, a):
         self.a = a
 
-    def __call__(self, kt, ku):
-        self.ku = ku
-        self.kt = kt
-
-        return -self.a*self.ku
+    def __call__(self, t, u):
+        return -self.a*u
 
     def solve(self, u0, T, dt):
-        self.u0 = u0
-        self.u = []
-        self.dt = dt
-        
-        for i in range(len(self.u0)):
-            self.t = [T[0]]
-            self.ii = 0
-            self.lu = [u0[i]]
-            if len(u0) == 1:
-                self.u.append(u0[i])
-            else:
-                self.u.append([u0[i]])
-
-            while self.t[self.ii] <= T[-1]:
-                du = self(self.t[self.ii], self.lu[self.ii])
-                if len(u0) == 1:
-                    du = self(self.t[self.ii], self.lu[self.ii])
-                    dlu = self.lu[self.ii] + du*self.dt
-                    self.u.append(dlu)
-                    self.lu.append(dlu)         
-                else:
-                    dlu = self.u[i][self.ii]+ du*self.dt
-                    self.u[i].append(dlu)
-                    self.lu.append(dlu)    
-                
-                self.t.append(self.t[self.ii] + self.dt)
-                self.ii += 1
-            
-        return self.t, self.u
+        sol = solve_ivp(self, T, u0, t_eval=np.arange(T[0], T[1] + dt, dt))
+        return sol.t, sol.y[0]
